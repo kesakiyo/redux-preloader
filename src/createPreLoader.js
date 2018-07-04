0316202c352253b21d3ff0ca9d68824c566e0499
Junbong Lee <koliie9039@gmail.com>
2018-07-04 20:08:24 +0900

    Add an option for determining whether show loader component always or not.

--- src/createPreLoader.js
+++ src/createPreLoader.js
@@ -3,6 +3,7 @@ import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { connect } from 'react-redux';
 import isFunction from 'lodash.isfunction';
+import isBoolean from 'lodash.isboolean';
 
 export default (defaults = {}) => {
   const getComponentName = component => (
@@ -24,6 +25,7 @@ export default (defaults = {}) => {
     const initializer = args.initializer || emptyFunction;
     const isLoading = args.isLoading || emptyFunction;
     const hasError = args.hasError || emptyFunction;
+    const showLoaderAlways = isBoolean(args.showLoaderAlways) ? args.showLoaderAlways : true;
     const LoadingComponent = args.LoadingComponent || defaults.DefaultLoadingComponent || emptyFunction;
     const ErrorComponent = args.ErrorComponent || defaults.DefaultErrorComponent || emptyFunction;
     const wrapperDisplayName = args.wrapperDisplayName || 'preLoader';
@@ -42,6 +44,7 @@ export default (defaults = {}) => {
         ...objectMap(args.injectToProps, state, ownProps),
         isLoading: isLoading(state, ownProps),
         hasError: hasError(state, ownProps),
+        showLoaderAlways,
       }))
       class preLoader extends React.Component {
         constructor() {
@@ -49,6 +52,7 @@ export default (defaults = {}) => {
           this.state = {
             forceUnmount: false,
           };
+          this.hasLoaded = false;
         }
 
         componentWillMount() {
@@ -74,7 +78,8 @@ export default (defaults = {}) => {
         }
 
         render() {
-          if (this.props.isLoading || this.state.forceUnmount) {
+          if (((!this.hasLoaded || this.props.showLoaderAlways) && this.props.isLoading) || this.state.forceUnmount) {
+            this.hasLoaded = true;
             return this.renderComponent(LoadingComponent);
           } else if (this.props.hasError) {
             return this.renderComponent(ErrorComponent);
